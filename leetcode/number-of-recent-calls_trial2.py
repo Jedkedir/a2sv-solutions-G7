1e3b6ba7f1919a2db6c7b6cd097360190f78e997
Yididiya Kedir <149969175+Jedkedir@users.noreply.github.com>
2026-04-07 20:18:21 +0300

    Add solution for Number of Recent Calls (trial 2)

---
+++ leetcode/number-of-recent-calls_trial2.py
@@ -0,0 +1,16 @@
+class RecentCounter:
+
+    def __init__(self):
+        self.requests = deque()
+
+    def ping(self, t: int) -> int:
+        self.requests.append(t)  
+        lowerBound = t - 3000
+        while self.requests and self.requests[0] < lowerBound:
+            self.requests.popleft()
+        return len(self.requests)      
+
+
+# Your RecentCounter object will be instantiated and called as such:
+# obj = RecentCounter()
+# param_1 = obj.ping(t)
